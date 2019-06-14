d812d4777666ad07bff9245c78721a9456efde24
Armin <arminale@yahoo.com>
2019-06-13 23:45:56 -0400

    added basic structure for routing

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const createApt = require("./schemas/apartment");
-createApt();
+const mongoose = require("mongoose");
+const express = require("express");
+const helmet = require("helmet");
+const debug = require("debug")("r:index.js");
+
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/roommates", { useNewUrlParser: true })
+  .then(() => debug("Connected to database"))
+  .catch(err =>
+    debug("Could not connect to database with error: " + err.message)
+  );
+app.use(helmet());
+app.use(express.json());
+
+const port = process.env.PORT | 3000;
+app.listen(port, () => debug(`Listening on port ${port}`));
