35c55bff8f870e16a4e0ba1a4268c7b5f217c287
Armin <arminale@yahoo.com>
2019-06-19 20:48:24 -0400

    starting refactoring

--- controllers/apartmentController.js
+++ controllers/apartmentController.js
@@ -2,66 +2,29 @@ const mongoose = require("mongoose");
 const debug = require("debug")("r:controller:apartment");
 const _ = require("lodash");
 const apartmentSchema = require("../schemas/apartment");
-const userSchema = require("../schemas/user");
-const transactionBucketSchema = require("../schemas/transactionBucket");
 
 const Apartment = mongoose.model("Apartment", apartmentSchema);
-const User = mongoose.model("User", userSchema);
-const TransactionBucket = mongoose.model(
-  "TransactionBucket",
-  transactionBucketSchema
-);
 
 async function createApartment(user, apartmentConfig) {
   debug(`Creating a new apartment for user: ${user.id}`);
-  const userDoc = await User.findOne({ _id: user.id });
-  if (!userDoc) {
-    debug(`No user found with id: ${user.id}`);
-    throw new Error(`No user found with id: ${user.id}`);
-  } else {
-    if (userDoc.apartment) {
-      debug(`User with id: ${user.id}  already has an apartment`);
-      throw new Error(`User with id: ${user.id}  already has an apartment`);
-    } else {
-      debug(`User with id:${user.id} found and has no apartment`);
-      const newBucket = await createTransactionBucket();
-      debug(`Created transaction bucket: ${JSON.stringify(newBucket)}`);
-      let apartment = new Apartment({
-        name: apartmentConfig.name,
-        members: [userDoc._id],
-        transactionBuckets: [newBucket._id],
-        debts: []
-      });
-      debug(`Creating apartment: ${JSON.stringify(apartment)}`);
-      apartmentDoc = await apartment.save();
-      debug(`Apartment saved as ${JSON.stringify(apartmentDoc)}`);
-      userDoc.apartment = apartmentDoc._id;
-      await userDoc.save();
-      return apartmentDoc;
-    }
-  }
-}
-
-async function createTransactionBucket() {
-  let beginBucketDate = new Date();
-  beginBucketDate.setUTCDate(1);
-  beginBucketDate.setHours(0, 0, 0, 0);
-  let transactionBucket = new TransactionBucket({
-    date: beginBucketDate,
-    transactions: []
+  let apartment = new Apartment({
+    name: apartmentConfig.name,
+    members: [user.id],
+    transactionBuckets: [],
+    debts: []
   });
-  transactionBucketDoc = await transactionBucket.save();
-  return transactionBucketDoc;
+  debug(`Creating apartment: ${JSON.stringify(apartment)}`);
+  apartmentDoc = await apartment.save();
+  debug(`Apartment saved as ${JSON.stringify(apartmentDoc)}`);
+  return apartmentDoc;
 }
 
-async function addUserToApartment(userId, apartmentIdString) {
-  let apartment = await Apartment.findById(
-    mongoose.Types.ObjectId(apartmentIdString)
-  );
+async function addUserToApartment(userId, apartmentId) {
+  let apartment = await Apartment.findById(apartmentId);
   if (_.find(apartment.members)) {
     return apartment;
   } else {
-    apartment.members.push(mongoose.Types.ObjectId(userId));
+    apartment.members.push(userId);
   }
   return await apartment.save();
 }
---
+++ controllers/transactionController.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const transactionBucketSchema = require("../schemas/transactionBucket");
+
+const TransactionBucket = new mongoose.model(
+  "Transaction Bucket",
+  transactionBucketSchema
+);
+
+async function createTransactionBucket() {
+  let beginBucketDate = new Date();
+  beginBucketDate.setUTCDate(1);
+  beginBucketDate.setHours(0, 0, 0, 0);
+  let transactionBucket = new TransactionBucket({
+    date: beginBucketDate,
+    transactions: []
+  });
+  transactionBucketDoc = await transactionBucket.save();
+  return transactionBucketDoc;
+}
